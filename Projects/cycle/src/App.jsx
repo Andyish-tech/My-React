cd00549e49579764a18a8ddff9bea65e4714fc68
Andyish-tech <ishmukazi@gmail.com>
2025-11-10 14:40:15 +0200

    Update App.jsx

--- Projects/cycle/src/App.jsx
+++ Projects/cycle/src/App.jsx
@@ -1,13 +1,21 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
+import ProductSection from "../src/components/ProductSection"
+import About from "../src/components/About"
+import Testimonial from "../src/components/Testimonial"
+import News from "../src/components/News"
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <ProductSection/>
+      <About />
+      <Testimonial />
+      <News />
       <Footer />
-    </>
+  </>
   );
 }
