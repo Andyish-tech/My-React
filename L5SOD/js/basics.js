143de89be9938246a91da54eb637d79af5e67949
Andyish-tech <ishmukazi@gmail.com>
2026-01-24 11:16:42 +0200

    Update basics.js

--- L5SOD/js/basics.js
+++ L5SOD/js/basics.js
@@ -1,4 +1,5 @@
-function login(age){
+function partReg(age){   
+
     if (age>=18 && age<=30){
         console.log("Old enough you are granted all features")
     }
@@ -13,4 +14,27 @@ function login(age){
     }
 }
 
-login(38)
+function login(email, Password){
+    
+    if(email=== "andy@gmail.com"){
+        if(Password=== 123){
+            console.log("Login Successfully")
+        }
+        else{
+            console.log("Incorrect Password")
+        }
+    }
+    else{
+        console.log("Incorrect email")
+    }
+}
+
+ function multTable(mult){
+    for(i=1; i<=10; i++){
+    result=mult*i;
+    console.log(`${mult} x ${i} = ${result}`);
+}
+ }
+multTable(6)
+
+
