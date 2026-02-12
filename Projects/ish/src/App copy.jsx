a2648fa827d5ba63fc07527899db9b64b3766394
Andyish-tech <ishmukazi@gmail.com>
2026-02-12 14:46:49 +0200

    Pages

---
+++ Projects/ish/src/App copy.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import './App.css'
+// import HOME from './components/Home'
+import Card from './components/Card'
+import Button from './components/Button'
+
+function App() {
+  const [post, setPost]=useState(null)
+  const [loading, setload]=useState(true)
+  const[error,setError]=useState()
+  fetch("https://jsonplaceholder.typicode.com/posts",{method:"GET"})
+  .then((posts)=>{
+    if(!posts.ok){
+      throw new Error('something went wrong')
+    }
+    return posts.json()
+  })
+  .then((data)=>{
+   
+    setPost(data)
+  })
+  .then(()=> setload(false))
+  .catch((err)=>setError(err.message))
+  return (
+    <>
+      <div className='container'>
+        {/* <HOME /> */}
+      {/* <Card title="Hey" description="Am happy to day" button1="Follow" > </Card> */}
+      <Card title="Hey" description="Am happy to day" button1="Click Me" button2="Follow" btndisplay={true}> </Card>
+      <Card title="Hey" description="Am happy to day" button1="Follow" btndisplay={true}> </Card>
+      <Button />
+      {/* <Obj /> */}
+      {
+        loading && <p>Loading...</p>
+      }
+      {
+        error && <p className='Error'>{error}</p>
+      }
+      {
+
+      post &&  post.map(post=>(
+          <Card title={post.title} description={post.body} button1={post.button1} button2="Follow" btndisplay={true}> </Card>
+        ))}
+
+      </div>
+    </>
+  )
+}
+
+export default App
--- Projects/ish/src/App.jsx
+++ Projects/ish/src/App.jsx
@@ -1,50 +1,27 @@
 import { useState } from 'react';
+import {BrowserRouter, Routes, Route}  from 'react-router-dom'
+
 import './App.css'
 // import HOME from './components/Home'
 import Card from './components/Card'
 import Button from './components/Button'
 
+
 function App() {
-  const [post, setPost]=useState(null)
-  const [loading, setload]=useState(true)
-  const[error,setError]=useState()
-  fetch("https://jsonplaceholder.typicode.com/postss",{method:"GET"})
-  .then((posts)=>{
-    if(!posts.ok){
-      throw new Error('something went wrong')
-    }
-    return posts.json()
-  })
-  .then((data)=>{
-   
-    setPost(data)
-  })
-  .then(()=> setload(false))
-  .catch((err)=>setError(err.message))
-  return (
+  return(
     <>
-      <div className='container'>
-        {/* <HOME /> */}
-      {/* <Card title="Hey" description="Am happy to day" button1="Follow" > </Card> */}
-      <Card title="Hey" description="Am happy to day" button1="Click Me" button2="Follow" btndisplay={true}> </Card>
-      <Card title="Hey" description="Am happy to day" button1="Follow" btndisplay={true}> </Card>
-      <Button />
-      {/* <Obj /> */}
-      {
-        loading && <p>Loading...</p>
-      }
-      {
-        error && <p className='Error'>{error}</p>
-      }
-      {
+    <BrowserRouter>
 
-      post &&  post.map(post=>(
-          <Card title={post.title} description={post.body} button1={post.button1} button2="Follow" btndisplay={true}> </Card>
-        ))}
+    <Routes>
 
-      </div>
+      <Route path='/Home' element={<Home />} />
+      <Route path='/About' element={<Card />} />
+      <Route path='/Services' element={<h1>Services</h1>} />
+            
+     </Routes>
+  
+     </BrowserRouter>
     </>
   )
 }
-
 export default App
