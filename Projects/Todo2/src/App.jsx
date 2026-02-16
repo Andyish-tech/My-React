72931fd2dfd52c7656d4ac658d7bab6409e54a5f
Andyish-tech <ishmukazi@gmail.com>
2026-02-16 13:55:52 +0200

    Delete task

--- Projects/Todo2/src/App.jsx
+++ Projects/Todo2/src/App.jsx
@@ -7,15 +7,28 @@ export default function App() {
   const[input,setInput]=useState('')
 
   function AddTask(){
-    const currentTask=[...tasks, {id:math.random(),name: input, checked:true}]
+    const currentTask=[...tasks, {id:Math.random(),name: input, checked:true}]
     setTask(currentTask);
     setInput("");
+  }
+  function Delete(id){
+    const ask= window.confirm("Are you sure you want to delete Task")
+    if(ask){
+      const newTask = tasks.filter(task => task.id !== id);
+      setTask(newTask)
+    }else{
+      return null
+    }
+  }
+
+  function chec(id){
+    const newtask
   }
   return (
     <div className='Container'>
       <div className="input">
         <h1>Todo</h1>
-        <input type="text" value={input} placeholder='New task' onChange={(e)=> setInput(e.target.value)}/><button onClick={AddTask}>Add</button>
+        <input type="text" value={input} placeholder='New task' onChange={(e)=> {setInput(e.target.value)}}/><button onClick={AddTask}>Add</button>
       </div>
       {
         tasks.map(task => (
@@ -24,7 +37,7 @@ export default function App() {
           <input type="checkbox" />
           <p>{task.name}</p>
         </div>
-        <button >Delete</button>
+        <button onClick={()=> Delete(task.id)} >Delete</button>
       </div>
         ))
       }
