7f0033f264fce9faccbf7dcf73fd0b24df515cf2
Andyish-tech <ishmukazi@gmail.com>
2026-02-16 16:11:53 +0200

    Localstorage

--- Projects/Todo2/src/App.jsx
+++ Projects/Todo2/src/App.jsx
@@ -1,10 +1,17 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 
 export default function App() {
-  const[tasks,setTask]= useState([])
+  const[tasks,setTask]= useState(JSON.parse(localStorage.getItem('tasks')) || [])
   const[input,setInput]=useState('')
+  const[search, setSearch]=useState("")
+
+  useEffect(()=>{
+    if(tasks.length>0){
+      localStorage.setItem('tasks', JSON.stringify(tasks))
+    }
+  },[tasks])
 
   function AddTask(){
     const currentTask=[...tasks, {id:Math.random(),name: input, checked:true}]
@@ -25,14 +32,19 @@ export default function App() {
     const newTask= [...tasks].map(t=> t.id ==id ? {...t, checked:!t.checked} : t)
     setTask(newTask)
   }
+
+  const filteredTasks=tasks.filter((t)=>t.name.toLowerCase().includes(search.toLowerCase()))
   return (
     <div className='Container'>
       <div className="input">
         <h1>Todo</h1>
         <input type="text" value={input} placeholder='New task' onChange={(e)=> {setInput(e.target.value)}}/><button onClick={AddTask}>Add</button>
       </div>
+      <div className="search">
+        <input type="text" onChange={(e)=>setSearch(e.target.value)}/>
+      </div>
       {
-        tasks.map(task => (
+        filteredTasks.map(task => (
         <div className="tasks">
         <div className="task-chec">
           <input type="checkbox" onChange={()=> chec(task.id)} checked={task.checked}/>
