235c4f94b442f0a5c9822900b50dd1145f7a970c
Andyish-tech <ishmukazi@gmail.com>
2026-02-16 13:38:46 +0200

    Adding Task

--- Projects/Todo2/src/App.jsx
+++ Projects/Todo2/src/App.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
+
 export default function App() {
+  const[tasks,setTask]= useState([])
+  const[input,setInput]=useState('')
+
+  function AddTask(){
+    const currentTask=[...tasks, {id:math.random(),name: input, checked:true}]
+    setTask(currentTask);
+    setInput("");
+  }
   return (
     <div className='Container'>
       <div className="input">
         <h1>Todo</h1>
-        <input type="text" placeholder='New task'/><button>Add</button>
+        <input type="text" value={input} placeholder='New task' onChange={(e)=> setInput(e.target.value)}/><button onClick={AddTask}>Add</button>
       </div>
-      <div className="tasks">
-        <input type="checkbox" />
-        <p></p>
+      {
+        tasks.map(task => (
+        <div className="tasks">
+        <div className="task-chec">
+          <input type="checkbox" />
+          <p>{task.name}</p>
+        </div>
         <button >Delete</button>
       </div>
+        ))
+      }
     </div>
   )
 }
