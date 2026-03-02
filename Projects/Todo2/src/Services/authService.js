9a2f7abf30ce35cc43b28e7b17e3cda83edf4faa
Andyish-tech <ishmukazi@gmail.com>
2026-03-02 15:30:49 +0200

    Create authService.js

---
+++ Projects/Todo2/src/Services/authService.js
@@ -0,0 +1,15 @@
+const authService={
+    register(data){
+        const{email, Username, Password}=data
+        if (!email, Username, Password){
+            return alert('all fields must be filled?');
+            const users= JSON.parse(localStorage.getItem('users'));
+            const exist = users?.some(u=> U.email==email);
+            if(exist){
+                return alert("User already exist");
+            }
+            const result = localStorage.setItem('users', JSON.stringify(data));
+            return{data, result};
+        }
+    }
+}
